38609d0d17219438f7bab7fb3eaa04f28bf42e79
Devansh Malik <devanshmalik2205@gmail.com>
2026-08-19 02:24:14 +0530

    Added package.json and basic config for Render_1

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "ExamNex backend is running!"
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
